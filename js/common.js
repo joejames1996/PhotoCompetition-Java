53106c9a5ab95be694c74f39553ab1874eaa2b61
Joe James <Joe.James@softwire.com>
2018-10-09 15:34:54 +0100

    Styles + voting up and down

--- js/common.js
+++ js/common.js
@@ -1,9 +1,20 @@
 'use strict';
 // JavaScript containing shared constants, for use in all pages.
 
-var backendIp = 'backend IP goes here';
-var token = 'your token goes here';
+var backendIp = '18.202.128.247';
+var token = '63fc9526-7aab-4665-a61f-31835bd7794c';
 
 function buildUrl(path) {
     return 'http://' + backendIp + '/images' + path + '?token=' + token;
+}
+
+function buildVoteUp(id)
+{
+	return buildUrl('/id/' + id + '/vote/up');
+	//return 'http://' + backendIp + 'images/id/' + id + 
+}
+
+function buildVoteDown(id)
+{
+	return buildUrl('/id/' + id + '/vote/down');
 }
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 // JavaScript for use with the index page.
 
+var id = -1;
+
 function loadRandomImage() {
     fetch(buildUrl('/random'))
         .then(function (response) {
@@ -13,15 +15,61 @@ function loadRandomImage() {
             console.log('Request to /random succeeded: ');
             console.log(json);
 
+            id = json.id;
+
             var mainImage = $('#main-image');
             mainImage.attr('src', json.url);
             mainImage.attr('alt', 'Photo Competition image, ' + json.name);
+
+            $('#picAuthor').html(json.author);
+            $('#picName').html(json.name);
+            $('#picLicense').html(json.license);
         })
         .catch(function (err) {
             console.error('Request to /random failed: ', err);
         });
 }
 
+function voteUp(id)
+{
+    fetch(buildVoteUp(id),
+    {
+        method: 'POST'
+    })
+        .then(function (response)
+        {
+            if(response.status !== 200)
+            {
+                throw new Error('Request return status code !== 200: ' + response.status + ' - ')
+            }
+        });
+}
+
+function voteDown(id)
+{
+    fetch(buildVoteDown(id),
+    {
+        method: 'POST'
+    })
+        .then(function (response)
+        {
+            if(response.status !== 200)
+            {
+                throw new Error('Request return status code !== 200: ' + response.status + ' - ')
+            }
+        });
+}
+
 $(function () {
     loadRandomImage();
+
+    $("button.vote-up").click(function()
+    {
+        if(id != -1) voteUp(id);
+    });
+
+    $("button.vote-down").click(function()
+    {
+        if(id != -1) voteDown(id);
+    });
 });
